1f58dc08d22b039a7584c414de21576aa809cb4b
ubuntu <aitechmanml@gmail.com>
2024-11-18 22:56:40 +0530

    hello

---
+++ ml/100days/feature-engineering.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler 
+from sklearn.preprocessing import MinMaxScaler
+
+
+# 1. Standardization
+
+df = pd.read_csv('./data/ads.csv')
+df.head()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
